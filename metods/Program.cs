277d72611c71ce7f7f72faa9613c477d757cc109
arina10006 <arina1006@mail.ru>
2017-03-01 11:53:16 +0600

    add mass and metod for random valyes

--- metods/Program.cs
+++ metods/Program.cs
@@ -8,8 +8,24 @@ namespace metods
 {
     class Program
     {
+        static void IntInMassByRandomValyes(int[] internel_mass, int max, int min)
+        {
+            Random gen = new Random();
+            for (int i = 0; i < internel_mass.Length; i++)
+            {
+                internel_mass[i] = gen.Next(min, max);
+            }
+        }
+
+        static void IntInMassByRandomValyes(int[] internel_mass)
+        { 
+
+        }
         static void Main(string[] args)
         {
+            int[] mas1;
+            mas1 = new int[10];
+
 
         }
     }
