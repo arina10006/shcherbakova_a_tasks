4942c049d9ce03357912f05acdedd4692ccf11fd
arina10006 <arina1006@mail.ru>
2017-03-01 13:20:30 +0600

    add 5 ex in Function and ex 2

---
+++ ex5_in_Function_basic_value_y/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex5_in_Function_basic_value_y
+{
+    class Program
+    {
+        static double Fraction(double numerator, double denominator)
+        {
+            double y= (numerator + Math.Sin(numerator))/(Math.Sin(denominator) + denominator);
+
+            return y;
+        }
+
+       
+        static void Main(string[] args)
+        {
+            double summ = 0;
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine("Введите значения для дроби {0}",i);
+                Console.WriteLine("Введитель число из числителя");
+                string a = Console.ReadLine();
+                double numerator = Convert.ToDouble(a);
+
+                Console.WriteLine("Введитель число из знаменателя");
+                string b = Console.ReadLine();
+                double denominator = Convert.ToDouble(b);
+
+                double y = Fraction(numerator, denominator);
+                
+                summ = summ + y;
+                
+            }
+
+            Console.WriteLine("Сумма у={0}", summ);
+            Console.ReadKey();
+
+        }
+    }
+}
