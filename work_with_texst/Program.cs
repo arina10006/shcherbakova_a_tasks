c790a114725ac7dae18b95eaf6e244581408b791
Buinovskaya Daria <dashk0oo@mail.ru>
2017-03-22 11:28:30 +0500

    add work with text

---
+++ work_with_texst/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work_with_texst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Public\Рабочий стол\Путин.txt");
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+        }
+    }
+}
