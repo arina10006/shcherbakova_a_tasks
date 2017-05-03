7da65376db6764b07f9740692f4d06c625b9caa7
arina10006 <arina1006@mail.ru>
2017-05-03 11:25:13 +0500

    add 3

---
+++ 03.05.string_ex25/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace _03._05.string_ex25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.
+        }
+    }
+}
