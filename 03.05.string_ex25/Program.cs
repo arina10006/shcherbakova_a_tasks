3578ea426a2212f35f3203fb5077a7ea9a3f78db
arina10006 <arina1006@mail.ru>
2017-05-03 12:06:57 +0500

    add ex26

--- 03.05.string_ex25/Program.cs
+++ 03.05.string_ex25/Program.cs
@@ -10,7 +10,18 @@ namespace _03._05.string_ex25
     {
         static void Main(string[] args)
         {
-            string text = File.
+            string text = File.ReadAllText(@"C:\Users\student\Desktop\PP.txt");
+
+            string[] masOfPeople = text.Split(new char[] {'.'});
+            for (int i = 0; i < masOfPeople.Length-1; i++)
+            {
+                string[] pp = masOfPeople[i].Split(';');
+                int a = Convert.ToInt32(pp[3]);
+                if (a<=2000)
+                {
+                    Console.WriteLine("Название оп: {0}\r\nЗавод: {1}\r\nГод выпуска: {2}\r\nЦена: {3} гривен\r\n", pp[0], pp[1], pp[2], pp[3]);
+                }
+            }
         }
     }
 }
