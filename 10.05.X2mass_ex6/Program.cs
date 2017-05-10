cc7eead65a87fe06a8e09458e54aa7d62ca9aad2
arina10006 <arina1006@mail.ru>
2017-05-10 11:21:06 +0500

    add 2 com

---
+++ 10.05.X2mass_ex6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._05.X2mass_ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrica = new int[4, 3];
+            Random gen = new Random();
+
+            for (int i = 0; i < matrica.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrica.GetLength(1); j++)
+                {
+                    matrica[i, j] = gen.Next(0, 9);
+                    Console.Write(matrica[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            int max1Index = 0, max3Index = 0;
+            for (int i = 1; i < matrica.GetLength(0); i++)
+            {
+                if (matrica[max1Index,0]< matrica[i,0])
+                {
+                    max1Index = i;
+                }
+                if (matrica[max3Index, 2] < matrica[i, 2])
+                {
+                    max3Index = i;
+                }
+            }
+            int change = matrica[max1Index,0];
+            matrica[max1Index,0] = matrica[max3Index,2];
+            matrica[max3Index,2] = change;
+            for (int i = 0; i < matrica.GetLength(0); i++)
+            {
+                for (int j = 0; j <  matrica.GetLength(1); j++)
+                {
+                    Console.Write(matrica[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 10.05.struct_ex16/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _10._05.struct_ex16
+{
+    struct HockeyPlayer
+    {
+        public string surname;
+        public int age;
+        public int play;
+        public int washer;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\student\Desktop\text.txt");
+
+            string[] man = text.Split(new char[] { '.' });
+            HockeyPlayer[] players = new HockeyPlayer[man.Length]; 
+            for (int i = 0; i < man.Length; i++)
+            {
+                string[] people = man[i].Split(new char[] { ';' });
+                players[i].surname = people[0];
+                players[i].age = Convert.ToInt32(people[1]);
+                players[i].play = Convert.ToInt32(people[2]);
+                players[i].washer = Convert.ToInt32(people[3]);
+            }
+            double midleAge = 0;
+            for (int i = 0; i < players.Length; i++)
+            {
+                midleAge = midleAge + players[i].age;
+            }
+            midleAge = midleAge / players.Length;
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (players[i].age<=25)
+                {
+                    Console.WriteLine("Фамилия:{0}\r\nВозраст:{1}\r\nКоличество игр:{2}\r\nЗабитые шайбы:{3}\r\n", players[i].surname, players[i].age,players[i].play, players[i].washer );
+                }
+                
+            }
+            
+        }
+    }
+}
