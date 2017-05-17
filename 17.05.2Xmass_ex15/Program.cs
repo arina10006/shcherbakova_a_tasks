553fe189e58d6960494f8780756b8f52b88b0c7b
arina10006 <arina1006@mail.ru>
2017-05-17 11:01:57 +0500

    17/05

---
+++ 17.05.2Xmass_ex15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace _17._05._2Xmass_ex15
+{
+    class Program
+    {
+        static int[][] ConversionToArray(string stringMass,  int p)
+        {
+            
+            string[] text = stringMass.Split(new char[] { ',' });
+            int[][] mass = new int[stringMass.Length][];
+            for (int j = 0; j < mass.GetLength(0); j++)
+            {
+                mass[p][j] = Convert.ToInt32(text[j]);   
+            }
+
+            return mass;
+        }
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\student\Desktop\mess.txt");
+            string[] mess = text.Split(new char[] { '.' });
+            
+            
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(mass[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 2Xmass__ex25/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Xmass__ex25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mass = new int[4, 4];
+            Random gen = new Random();
+            for (int i = 0; i < mass.GetLength(0); i++)
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    mass[i, j] = gen.Next(-10, 5);
+                    Console.Write(mass[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            int negativeElements = 0;
+            for (int i = 0; i < mass.GetLength(1); i++)
+            {
+                if (mass[i, 1] < 0)
+                {
+                    negativeElements = negativeElements + 1;
+                }
+            }
+            Console.WriteLine("Количество отрицательных элементов во 2-м столбце = {0}", negativeElements);
+        }
+    }
+}
