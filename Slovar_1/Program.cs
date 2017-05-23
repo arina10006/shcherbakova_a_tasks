3cb9fe57a899afdea12da908f5b7039289a8f77b
HP <HP@HP-HP>
2017-05-23 16:31:29 +0500

    23.05

---
+++ Slovar_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Slovar_1
+{
+    class Program
+    {
+        static bool SearchInUnique(string uniqueStr, char letter)
+        {
+            for (int i = 0; i < uniqueStr.Length; i++)
+            {
+                if (uniqueStr[i] == letter)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+
+        }
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\HP\Desktop\news.txt");
+            text.ToLower();
+
+            string unic = "";
+
+            foreach (char i in text)
+            {
+                if (unic.Length == 0)
+                {
+                    unic = unic + i;
+                }
+                else
+                {
+                    bool lokal = SearchInUnique(unic, i);
+                    if (lokal)
+                    {
+                        unic = unic + i;
+
+                    }
+                }
+            }
+            Dictionary <string, int> dictionary = new Dictionary<string, int>();
+            
+        }
+    }
+}
---
+++ example for summ/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("example for summ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("example for summ")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("cae01a50-77a3-4fb5-ac27-03e7d786b2cc")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
